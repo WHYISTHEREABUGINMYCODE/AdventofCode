86324aff39a0fdc4c58e06ac17643cdfa5cf09e8
Louis Yamada <booiboilouis@Louiss-MacBook-Air.local>
2022-12-16 16:53:10 +0900

    change structure

--- 2022/common.py
+++ 2022/common.py
@@ -1,5 +1,5 @@
 import os
 # gets file relative to inputs directory (e.g. pass in 'day1/input.txt')
 def read_input(filepath):
-    with open(os.getcwd() + "/inputs/" + filepath, "r") as f:
+    with open(os.getcwd() + "/2022/inputs/" + filepath, "r") as f:
         return f.read()
